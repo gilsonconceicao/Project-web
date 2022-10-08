99559e29203429978cd3916ba0c0c0118174dfcf
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2022-10-07 23:57:16 -0300

    functions and new scripts

--- src/js/searchCep.js
+++ src/js/searchCep.js
@@ -1,11 +1,24 @@
-const seacrhingCep = async () => {
-    const cep = document.getElementById('searchCep').value; 
-    const url = `https://viacep.com.br/ws/${cep}/json/`; 
+const btn = document.getElementById('btn-submit')
+    .addEventListener('click', (e) => {
+        e.preventDefault(); 
 
-
-    fetch(url) 
-        .then(response => response.json())
-        .then((data) => {
-            
-        }).catch(e=> alert(e))
-}
+        const cep = document.getElementById('searchCep').value; 
+        const url = `https://viacep.com.br/ws/${cep}/json/`; 
+    
+    
+        fetch(url) 
+            .then(response => response.json())
+            .then((data) => {    
+                const div = document.createElement('div'); 
+                div.setAttribute('class', 'data-cep')
+                const result = document.querySelector('.result');
+    
+                div.innerHTML = `
+                    <h2>CEP: ${data.cep}</h2> 
+                    <span>Rua: ${data.logradouro}</span> <br>
+                    <span>Bairro: ${data.bairro}</span> <br>
+                    <span>Cidade: ${data.localidade} - ${data.uf}</span> 
+                `; 
+                result.appendChild(div)
+            }).catch(e=> alert(e))
+    })
---
+++ src/js/showListUser.js
@@ -0,0 +1,19 @@
+// data storage show html
+const dataLst = document.getElementById('data_list'); 
+// get data storage 
+const storageList = JSON.parse(localStorage.getItem('listTasksCurrent')); 
+
+storageList.forEach(list => {
+    dataLst.innerHTML += `
+        <div class='tasks'>
+            <h2>${list.title}</h2>
+            <span class='description'>${list.description}</span>
+            <span class='date'>Adicionado no dia ${list.date}</span>
+        </div>  
+    ` 
+})
+
+document.querySelector('.removeList').addEventListener('click', () => {
+    window.localStorage.removeItem('listTasksCurrent'); 
+    window.location.href = ''
+})
